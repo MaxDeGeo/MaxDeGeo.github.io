ae8cc3e758206a4a1cea534b2deaf503a63ba055
Max De George <mtd3442@rit.edu>
2018-04-03 22:57:44 -0400

    Added jQuery because I realized it was insane to not use it, and it makes my life easier. Added the fade in and fade out effects for Experience.

--- js/experience.js
+++ js/experience.js
@@ -10,45 +10,27 @@ function closeNav() {
 
 //For displaying content about each level.
 function display(x) {
-
+    
     let elements = document.querySelectorAll("#information .info")
     let element = elements[x];
     let close = document.querySelectorAll(".close");
 
-
+    //Removes any element that hasn't been removed by the user.
     for(let z = 0; z < elements.length; z++)
     {
-
-        //console.log(elements[x].children[1].children[elements[x].children[1].children.length - 2]);
-        //console.log(elements[x].children[1].children[elements[x].children[1].children.length - 2].style.display);
-        //if(elements[x].children[1].children[elements[x].children[1].children.length - 2].style.display !== "none")
-        //if(elements[x].children[1].children[7].style.display !== "none")
-    
-        console.log(close[z].style.display);
-
         if(close[z].style.display === "block")
         {
-            console.log("Entered");
             elements[z].style.display = "none";
         }
     }
 
-    element.children[0].style.display = "none";
-
-    for(let y = 0; y < element.children[1].children.length - 3; y++)
+    //Fades in the element for display
+    if(element.style.display !== "block")
     {
-        element.children[1].children[y].style.display = "none";
+        $(element).stop().fadeIn(250).css("display","block");
     }
 
-    element.children[1].children[element.children[1].children.length - 1].style.display = "none";
-
-    
-    element.style.display = "block";
-    close[x].style.display = "block";
-    element.style.WebkitTransition = "all 2s";
-    element.style.transition = "all 2s";
-    
-
+    $(close[x]).stop().fadeIn(250).css("display","block");
 }
 
 //For removing content 
@@ -57,5 +39,5 @@ function disable(x) {
     let elements = document.querySelectorAll("#information .info");
     let element = elements[x];
 
-    element.style.display = "none";
+    $(element).stop().fadeOut();
 }

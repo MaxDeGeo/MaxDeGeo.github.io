6c8f222a0570784380c79c36d4fb9c917749d094
Max De George <mtd3442@rit.edu>
2018-04-04 22:20:27 -0400

    Cleaned up contact screen for desktop display only

--- js/contact.js
+++ js/contact.js
@@ -6,3 +6,19 @@ function openNav() {
 function closeNav() {
     document.querySelector("#responsive-nav nav").style.width = "0px";
 }
+
+function validEmail() {
+    let email1 = document.querySelector("#email");
+    let email2 = document.querySelector("#email2");
+
+    console.log(email1);
+
+    if(email1 !== email2)
+    {
+        
+    }
+    else
+    {
+
+    }
+}

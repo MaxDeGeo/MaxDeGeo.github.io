5c3e63c5ad509288bc2139fbfd5480dfa7928eb3
Max De George <mtd3442@rit.edu>
2018-04-06 02:15:01 -0400

    Added custom error messages for form.

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,13 @@
+window.onload = function() {
+    
+    let submit = false;
+    let firstName = document.querySelector("#firstName");
+    let secondName = document.querySelector("#lastName");
+    let email = document.querySelector("#email");
+    let email2 = document.querySelector("#email2");
+
+}
+
 function openNav() {
     document.querySelector("#responsive-nav nav").style.width = "250px";
 
@@ -7,18 +17,62 @@ function closeNav() {
     document.querySelector("#responsive-nav nav").style.width = "0px";
 }
 
-function validEmail() {
-    let email1 = document.querySelector("#email");
-    let email2 = document.querySelector("#email2");
-
-    console.log(email1);
-
-    if(email1 !== email2)
+function displayError(element) {
+    $(email2).parent().children().fadeIn().css("display", "inline-block");
+    email2.parentElement.children[0].children[0].style.cursor = "pointer";
+    $(email2.parentElement.children[0]).hover(function()
     {
-        
-    }
-    else
+        email2.parentElement.children[1].style.visibility = "visible";
+    }, function() 
     {
+        email2.parentElement.children[1].style.visibility = "hidden";
+    });
+} 
 
+function customErrorMessage() {
+    
+    if(email.value !== email2.value)
+    {
+        displayError(email2)
+        email2.parentElement.children[1].innerHTML = "Emails are not the same.";
     }
+
+    if($(firstName).val())
+    {
+        displayError(firstName)
+        firstName.parentElement.children[1].innerHTML = "Please enter your first name.";
+    }
+
+    if($(lastName).val())
+    {
+        displayError(lastName)
+        lastName.parentElement.children[1].innerHTML = "Please enter your last name.";
+    }
+}
+
+function canSubmit() {
+    
+    submit = false;
+
+    if(firstName.value !== null)
+    {
+        if(lastName.value !== null)
+        {
+            if(email.value !== null)
+            {
+                if(email2.value !== null)
+                {
+                    submit = true;
+                }
+            }
+        }
+    }
+
+    if(email.value !== email2.value)
+    {
+        submit = false;
+        customErrorMessage();
+    }
+
+    return submit;
 }

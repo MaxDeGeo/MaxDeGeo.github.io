ac2ada89c5b0691e3e84f99b0e7ed4b8fbeff4b2
Max De George <maxdegeorge@Maxs-MacBook-Pro.local>
2020-08-11 18:10:03 -0400

    Updated resume and made profile picture faster

---
+++ build/precache-manifest.095a2920c9c637f36197c8588269ea4d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb210135a588f8148816681705cf3fac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e924914792a6615406a",
+    "url": "/static/css/main.fd9d790f.chunk.css"
+  },
+  {
+    "revision": "9c44200e6d58738867b9",
+    "url": "/static/js/2.5bcbaefa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5bcbaefa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e924914792a6615406a",
+    "url": "/static/js/main.1d409921.chunk.js"
+  },
+  {
+    "revision": "0b9b2e19989ea433e772",
+    "url": "/static/js/runtime-main.016a6d70.js"
+  },
+  {
+    "revision": "e8460f0d8b9df01e9460bfd1ade0e8c8",
+    "url": "/static/media/Csharp.e8460f0d.png"
+  },
+  {
+    "revision": "614e81bd92afc62be8127beac5e1b9af",
+    "url": "/static/media/cpp.614e81bd.png"
+  },
+  {
+    "revision": "eb7f46922e7cca191013391fc25063ff",
+    "url": "/static/media/css3.eb7f4692.png"
+  },
+  {
+    "revision": "1f176829b81637c6fbd49fd5a32919ab",
+    "url": "/static/media/degeorge_max.1f176829.pdf"
+  },
+  {
+    "revision": "01afa4df934e6617c5fe9ac24752a1be",
+    "url": "/static/media/developer.01afa4df.png"
+  },
+  {
+    "revision": "b67f10044e1189a764815d4c687206b5",
+    "url": "/static/media/itchio.b67f1004.png"
+  },
+  {
+    "revision": "4029c3cd54af4e3dd7e7c14d30c3be7f",
+    "url": "/static/media/java.4029c3cd.png"
+  },
+  {
+    "revision": "b095413e37cc29ade7d14ad0d901ce8d",
+    "url": "/static/media/js.b095413e.png"
+  },
+  {
+    "revision": "446763959684b108ca164f3cdc9c96d0",
+    "url": "/static/media/photoshop.44676395.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "cb75cc0083b5ccedbb4541df0ea89439",
+    "url": "/static/media/ruby.cb75cc00.png"
+  },
+  {
+    "revision": "b420873f145ebb033170e23384df9546",
+    "url": "/static/media/rubymine.b420873f.png"
+  },
+  {
+    "revision": "db95c7a5fdfea6778ae62adce7a5196a",
+    "url": "/static/media/swift.db95c7a5.png"
+  },
+  {
+    "revision": "c9c45e6966c65d1c38df77239e8eaa69",
+    "url": "/static/media/unity.c9c45e69.png"
+  },
+  {
+    "revision": "d72b6cd11bfae1020be5963507a4679c",
+    "url": "/static/media/vs.d72b6cd1.png"
+  },
+  {
+    "revision": "225fbbe09689a4a3219ae1e00fe2280b",
+    "url": "/static/media/vscode.225fbbe0.png"
+  },
+  {
+    "revision": "600a107c497d7c14175906414b07c3d6",
+    "url": "/static/media/xcode.600a107c.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff5e7823b88b6f49c723af5fe2f0499b.js"
+  "/precache-manifest.095a2920c9c637f36197c8588269ea4d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
